f8d2ba6acb7ec495dbb06ab08760f576d07f85bf
torkil-kop <torkil.kop@gmail.com>
2023-01-30 16:44:48 +0300

    new task2

--- 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -6,11 +6,25 @@ import java.util.List;
 Знакомство с дженериками
 */
 
-public class Solution {
-    public static class SomeClass {
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static class SomeClass<T extends Number> {
     }
 
     public static void main(String[] args) {
 
     }
 }
+/*
+Знакомство с дженериками
+Параметризируй классы SomeClass и Solution следующим образом:
+1. SomeClass должен работать с типами, которые наследуются от Number;
+2. Solution должен работать с типами, которые наследуются от List, который в свою очередь параметризируется типом SomeClass.
+
+
+Requirements:
+1. Класс SomeClass должен работать с типами которые наследуются от Number.
+2. Класс Solution должен работать с типами, которые наследуются от List, который в свою очередь параметризируется типом SomeClass.
+3. Класс SomeClass должен быть публичным.
+4. Класс SomeClass должен быть статическим.
+5. Класс SomeClass должен находиться внутри класса Solution.
+ */
