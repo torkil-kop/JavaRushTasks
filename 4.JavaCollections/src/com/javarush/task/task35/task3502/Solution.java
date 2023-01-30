67a4558ed94daea4ecd6ad843e5ac1ee65162d0b
torkil-kop <torkil.kop@gmail.com>
2023-01-30 16:36:35 +0300

    new task

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task35.task3502;
+
+import java.util.List;
+
+/* 
+Знакомство с дженериками
+*/
+
+public class Solution {
+    public static class SomeClass {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
