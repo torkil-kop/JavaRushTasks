4f54cc011c20721595a268dadf707740a80a5b6e
torkil-kop <torkil.kop@gmail.com>
2023-01-30 16:31:39 +0300

    test1

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
@@ -7,9 +7,6 @@ package com.javarush.task.task35.task3501;
 public class Solution {
     public static void main(String[] args) {
         Number number = (Number) GenericStatic.<Number>someStaticMethod(new Integer(3));
-        System.out.println("Ghjdthrf");
-        System.out.println("23332222");
-        System.out.println("fwgg");
-        System.out.println("225");
+
     }
 }
