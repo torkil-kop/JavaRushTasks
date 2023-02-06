39b51c22d03e77c15c8b754bca06c3dab39f2ba5
torkil-kop <torkil.kop@gmail.com>
2023-02-06 12:23:16 +0300

    2

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+public class Shortener {
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
+public synchronized Long getId(String string){
+        if (storageStrategy.containsValue(string)){
+            return storageStrategy.getKey(string);
+        }
+        lastId++;
+        storageStrategy.put(lastId, string);
+        return lastId;
+}
+
+
+
+    public synchronized String getString(Long id){
+
+            return storageStrategy.getValue(id);
+
+    }
+    /*
+    3.5. Реализуй метод getString, он должен вернуть строку по заданному идентификатору (ключу).
+3.6. Предусмотреть возможность вызова методов getId и getString из разных потоков добавив соответствующий модификатор к заголовкам методов.
+     */
+}
