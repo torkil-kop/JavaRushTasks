e1d5be804ac6ac6552327870ef5ec3d834900f57
torkil-kop <torkil.kop@gmail.com>
2023-02-08 17:34:12 +0300

    08.02.23

--- 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 
 public class Solution<T> extends ArrayList<T> {
     public static void main(String[] args) {
+
         Solution<Integer> list = new Solution<>();
         list.add(1);
         list.add(2);
@@ -25,8 +26,98 @@ public class Solution<T> extends ArrayList<T> {
                 break;
             }
         }
+
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
     }
 
-    public class RoundIterator {
+    public class RoundIterator implements Iterator<T> {
+        int cursor;       // index of next element to return
+        int lastRet = -1; // index of last element returned; -1 if no such
+        int expectedModCount = modCount;
+
+        public boolean hasNext() {
+            return size() > 0;
+        }
+
+        @SuppressWarnings("unchecked")
+        public T next() {
+            checkForComodification();
+            int i = cursor;
+            if (i >= size())
+                i = 0;
+            //private transient Object[] elementData;
+            Object[] elementData = null;
+            try {
+                Field elementDataField = ArrayList.class.getDeclaredField("elementData");
+                elementDataField.setAccessible(true);
+                elementData = (Object[]) elementDataField.get(Solution.this);
+
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+            }
+            if (i >= elementData.length)
+                throw new ConcurrentModificationException();
+            cursor = i + 1;
+            return (T) elementData[lastRet = i];
+        }
+
+        public void remove() {
+            if (lastRet < 0)
+                throw new IllegalStateException();
+            checkForComodification();
+
+            try {
+                Solution.this.remove(lastRet);
+                cursor = lastRet;
+                lastRet = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+        }
+
     }
 }
+/*
+public class SOList<Type> implements Iterable<Type> {
+
+    private Type[] arrayList;
+    private int currentSize;
+
+    public SOList(Type[] newArray) {
+        this.arrayList = newArray;
+        this.currentSize = arrayList.length;
+    }
+
+    @Override
+    public Iterator<Type> iterator() {
+        Iterator<Type> it = new Iterator<Type>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < currentSize && arrayList[currentIndex] != null;
+            }
+
+            @Override
+            public Type next() {
+                return arrayList[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+}
+ */
